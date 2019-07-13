ae6c2fc47a33006a7dfcb2457833917ef937951b
chris <930193463@qq.com>
2019-07-13 13:09:53 +0800

    sss

--- src/main/java/com/qf/realms/MyRealm.java
+++ src/main/java/com/qf/realms/MyRealm.java
@@ -22,6 +22,7 @@ public class MyRealm extends AuthorizingRealm {
         Set<String> roles = new HashSet<String>();
         roles.add("role1");
         roles.add("role2");
+
         //根据角色列表查询数据库中的对应的权限列表
         Set<String> permissions = new HashSet<String>();
         permissions.add("user:add");
